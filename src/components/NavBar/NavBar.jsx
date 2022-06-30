e78dad48a28fb9a0561eaa877c84b0593b3b35d0
DESKTOP-H6VSNNL\Lenovo <kubaaml1@gmail.com>
2022-06-30 14:14:00 +0200

    dsad

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,12 +5,12 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className='NavBar'>
-        <div className="logo">LOGO</div>
         <div className="links">
             <Link to='/'>Główna</Link>
             <Link to='/order'>Zamów jedzenie</Link>
             <Link to='/log-in'>Zaloguj się</Link>
             <Link to='/admin-panel'>Panel administratora</Link>
+            <div className="dot"></div>
         </div>
     </div>
   )
