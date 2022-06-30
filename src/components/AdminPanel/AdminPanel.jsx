0b6ad9f62a8ea2151bd0b80abe7364934af76ab6
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-06-30 13:10:42 +0200

    a

--- src/components/AdminPanel/AdminPanel.jsx
+++ src/components/AdminPanel/AdminPanel.jsx
@@ -5,6 +5,7 @@ const AdminPanel = () => {
   return (
     <div className='AdminPanel'>
         <nav className='navigation'>
+            <div className="title">Admin Panel</div>
             <button>Restauracje</button>
             <button>Użytkownicy</button>
         </nav>
