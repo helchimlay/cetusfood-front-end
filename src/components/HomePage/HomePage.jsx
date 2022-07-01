19b4eeb61168d511ca353dc48702d9e00a1801f8
Wojtek <weichergames345@gmail.com>
2022-07-01 11:41:14 +0200

    wstępny homepage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './HomePage.css';
+import cetusek from '../../images/CetusFoodNapisDown.png';
 
 const HomePage = () => {
   return (
     <div className='HomePage'>
-        home
+        <div className="cetusek"><img src={cetusek} alt="cetusek" /></div>
+        <div className="about-us">
+          <h2>O Aplikacji</h2>
+          <p>Nasza dynamicznie rozwijająca się aplikacja Cetusfood, pomoże Ci
+złożyć zamówienie jedzenia dla całej firmy. Aplikacja została
+stworzona przez stażystów z Zespołu Szkół Technicznych.
+Naszym celem było stworzenie aplikacji, która ma za zadanie
+usprawnić funkcjonowanie pracy w firmie.</p>
+        </div>
     </div>
   )
 }
