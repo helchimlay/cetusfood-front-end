5a59791633da298398330c7693fa070ac8e59f23
Wojtek <weichergames345@gmail.com>
2022-07-01 21:08:37 +0200

    zmiany

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import './HomePage.css';
 import cetusek from '../../images/CetusFoodNapisDown.png';
+import cetKurier from '../../images/CetusikKurier.png';
 
 const HomePage = () => {
   return (
     <div className='HomePage'>
+      <section>
+        <div className="aboutUs">
         <div className="cetusek"><img src={cetusek} alt="cetusek" /></div>
-        <div className="about-us">
+        <div className="graybox about-us">
           <h2>O Aplikacji</h2>
           <p>Nasza dynamicznie rozwijająca się aplikacja Cetusfood, pomoże Ci
-złożyć zamówienie jedzenia dla całej firmy. Aplikacja została
-stworzona przez stażystów z Zespołu Szkół Technicznych.
-Naszym celem było stworzenie aplikacji, która ma za zadanie
-usprawnić funkcjonowanie pracy w firmie.</p>
+              złożyć zamówienie jedzenia dla całej firmy. Aplikacja została
+              stworzona przez stażystów z Zespołu Szkół Technicznych.
+              Naszym celem było stworzenie aplikacji, która ma za zadanie
+              usprawnić funkcjonowanie pracy w firmie.</p>
         </div>
+      </div>
+      <hr />
+        <div className="offer">
+          <div className="graybox whatOffer">
+            <h2>Co oferujemy?</h2>
+            <p>Dostarczamy zamówienia z najlepszych restauracji i barów.
+              W naszej ofercie są produkty z m.in.: McDonald's, KFC, Burger King, Subway, Pizza Hut, Starbucks i wiele więcej... 
+            </p>
+          </div>
+          <div className="cetKurier"><img src={cetKurier} alt="cetKurier" /></div>
+        </div>
+      </section>
+      
     </div>
   )
 }
