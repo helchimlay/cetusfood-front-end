acb7e3ceab929bba5aaf036d749e3290d2b4c8e5
mietek442 <96135826+mietek442@users.noreply.github.com>
2022-07-01 08:53:02 +0200

    zcxzzx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './NavBar.css';
 import { Link } from 'react-router-dom';
-
+import { click } from '@testing-library/user-event/dist/click';
 const NavBar = () => {
+  const handleClick = () => {
+    console.log('adma jest najlepszy')
+    document.getElementById('ada').style.backgroundColor="red";
+  }
   return (
     <div className='NavBar'>
         <div className="links">
-            <Link to='/'>Główna</Link>
-            <Link to='/order'>Zamów jedzenie</Link>
-            <Link to='/log-in'>Zaloguj się</Link>
-            <Link to='/admin-panel'>Panel administratora</Link>
-            <div className="dot"></div>
+            <Link to='/'>Główna <div className="dot"></div></Link>
+            <Link to='/order'>Zamów jedzenie <div className="dot"></div></Link>
+            <Link to='/log-in'>Zaloguj się <div className="dot"></div></Link>
+            <Link id='ada'to='/admin-panel'>Panel administratora <div className="dot"></div></Link>
         </div>
+        <button onClick={handleClick}>adam</button>
     </div>
   )
 }
