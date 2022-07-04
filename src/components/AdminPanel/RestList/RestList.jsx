3e1d67132a3e63d1b16a0a764d28d587dcf5cb68
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-04 08:51:20 +0200

    robie nowy adminpanel

--- src/components/AdminPanel/RestList/RestList.jsx
+++ src/components/AdminPanel/RestList/RestList.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import NewRestForm from './NewRestForm';
 import './RestList.css';
-import RestListEl from './RestListEl';
-import { getRestaurants, deleteRestaurant } from '../../services/RestaurationsList';
+import { getRestaurants, getRestaurantsByName, deleteRestaurant } from '../../services/RestaurationsList';
 
 
 export const RestListCtx = React.createContext();
@@ -21,9 +19,8 @@ const RestList = () => {
     const searchBarRef = React.useRef(null);
 
     const handleSearch = ()=>{
-        // fechuj i wepchaj do fetchedData z searchBarRef
         if(searchBarRef.current.value !== ''){
-            alert('szukam');
+            getRestaurantsByName(searchBarRef.current.value).then(data => setFetchedData(data.data));
         }
     }
 
@@ -33,68 +30,7 @@ const RestList = () => {
                 selectedElId: selectedElId, setSelectedElId: setSelectedElId,
                 newRestFormData: newRestFormData, setNewRestFormData: setNewRestFormData
             }}>
-                <div className="list">
-                    <div className='filtres'>
-                        <div className='search-field'>
-                            <input ref={searchBarRef} type="text" placeholder='Szukaj po nazwie' />
-                            <i className='bx bx-search' onClick={handleSearch}></i>
-                        </div>
-                    </div>
-                    <div className="table" style={{height: '65vh'}}>
-                        <table>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Lp.</th>
-                                    <th>Nazwa</th>
-                                    <th>Link</th>
-                                    <th>Email</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {fetchedData?.map((el, i) => (
-                                    <RestListEl key={i} id={el.id} oNum={i + 1} name={el.name} link={el.url} email={el.email} />
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                    <div className="add-new-form">  
-                        <button onClick={()=>{setNewRestFormOpened(true); setSelectedElId(null)}}>Dodaj nowy element</button>
-                    </div>
-                </div>
-                <div className="options">
-                    {selectedElId ? (<>
-                        <h4 className='title'>Opcje, id: {selectedElId}</h4>
-                        <div className="option">
-                            <p>
-                                <span>Edytuj element</span>
-                                <button>Akceptuj zmiany</button>
-                            </p>
-                            <p>
-                                <label htmlFor="name">Nazwa: </label>
-                                <input placeholder='nazwa...' type="text" name="name" id="name" autoComplete='off' />
-                            </p>
-                            <p>
-                                <label htmlFor="email">Email: </label>
-                                <input placeholder='email...' type="email" name="email" id="email" autoComplete='on' />
-                            </p>
-                            <p>
-                                <label htmlFor="link">Link: </label>
-                                <input placeholder='link...' type="text" name="link" id="link" autoComplete='on' />
-                            </p>
-                        </div>
-                        <div className="option">
-                            <p>
-                                <span>Usuń element</span>
-                                <button onClick={()=>deleteRestaurant(selectedElId)}>Usuń</button>
-                            </p>
-                        </div>
-                    </>) : (newRestFormOpened ? (
-                        <NewRestForm />
-                    ) : (
-                        <><h4 className='title'>Nie wybrano elementu</h4></>
-                    ))}
-                </div>
+                
             </RestListCtx.Provider>
         </div>
     )
--- src/components/services/RestaurationsList.js
+++ src/components/services/RestaurationsList.js
@@ -2,6 +2,9 @@ import axios from "axios";
 export const getRestaurants = async ()=>{
     return await axios.get('http://192.168.0.197:3000/restaurants');
 }
+export const getRestaurantsByName = async (name)=>{
+    return await axios.get(`http://192.168.0.197:3000/restaurants/name/${name}`);
+}
 export const deleteRestaurant = async (id)=>{
     await axios.delete(`http://192.168.0.197:3000/restaurants/${id}`);
 }
@@ -9,5 +12,4 @@ export const addRestaurant = async (data)=>{
     if(data.name && data.url && data.email){
         await axios.post('http://192.168.0.197:3000/restaurants', data);
     }
-    console.log(data)
 }
