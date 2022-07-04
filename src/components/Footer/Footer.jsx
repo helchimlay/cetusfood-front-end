70e9f4aef7d1a3b385edd5ea4f1825f422693292
mietek442 <96135826+mietek442@users.noreply.github.com>
2022-07-04 09:27:33 +0200

    saddsasafsaf

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='Footer'>
-        <div class="footer-left">
+        <div className='footer-left'>
 
 <h3>Cetus<span>Food</span></h3>
 
-<p class="footer-links">
+<p className='footer-links'>
   <a href="#">Home</a>
   ·
   <a href="#">Blog</a>
@@ -22,42 +22,42 @@ const Footer = () => {
   <a href="#">Contact</a>
 </p>
 
-<p className="footer-company-name">CetusFood &copy; 2022</p>
+<p className='footer-company-name'>CetusFood &copy; 2022</p>
 </div>
 
-<div className="footer-center">
+<div className='footer-center'>
 
 <div>
-  <i className="fa fa-map-marker"></i>
+  <i className='fa fa-map-marker'></i>
   <p><span>Rzeszów </span> PL(Poland)</p>
 </div>
 
 <div>
-  <i className="fa fa-phone"></i>
+  <i className='fa fa-phone'></i>
   <p>+48 886 668 544</p>
 </div>
 
 <div>
-  <i className="fa fa-envelope"></i>
+  <i className='fa fa-envelope'></i>
   <p><a href="mailto:support@company.com">fantacydesignss@gmail.com
   </a></p>
 </div>
 
 </div>
 
-<div className="footer-right">
+<div className='footer-right'>
 
-<p class="footer-company-about">
+<p className='footer-company-about'>
   <span>About the company</span>
   we are the company through which you order food.
 </p>
 
-<div className="footer-icons">
+<div className='footer-icons'>
 
-  <a href="#"><i class="fab fa-facebook-f"></i></a>
-  <a href="#"><i class="fa fa-twitter"></i></a>
-  <a href="#"><i class="fa fa-linkedin"></i></a>
-  <a href="#"><i class="fa fa-github"></i></a>
+  <a href="#"><i className='bx bxl-facebook-circle'></i></a>
+  <a href="#"><i className='bx bxl-twitter'></i></a>
+  <a href="#"><i className='bx bxl-instagram' ></i></a>
+  <a href="#"><i className='bx bxl-linkedin' ></i></a>
 
 </div>
 
