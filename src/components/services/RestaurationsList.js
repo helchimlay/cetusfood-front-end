151af2a61142684d9842b4decf1c7b22389ec85d
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-05 12:58:31 +0200

    a

--- src/components/services/RestaurationsList.js
+++ src/components/services/RestaurationsList.js
@@ -1,15 +1,15 @@
 import axios from "axios";
-export const getRestaurants = async ()=>{
-    return await axios.get('/restaurants');
+export const getRestaurants = async () => {
+    return await axios.get('http://192.168.0.246:3000/restaurants');
 }
-export const getRestaurantsByName = async (name)=>{
-    return await axios.get(`/restaurants/name/${name}`);
+export const getRestaurantsByName = async (name) => {
+    return await axios.get(`http://192.168.0.246:3000/restaurants/name/${name}`);
 }
-export const deleteRestaurant = async (id)=>{
-    await axios.delete(`/restaurants/${id}`);
+export const deleteRestaurant = async (id) => {
+    await axios.delete(`http://192.168.0.246:3000/restaurants/${id}`);
 }
-export const addRestaurant = async (data)=>{
+export const addRestaurant = async (data) => {
     if(data.name && data.url && data.email){
-        await axios.post('/restaurants', data);
+        await axios.post('http://192.168.0.246:3000/restaurants', data);
     }
 }
