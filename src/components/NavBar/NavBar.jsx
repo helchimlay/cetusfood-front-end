9fdf60c4921404d6877b33536fb78b7cf85108ff
Wojtek <weichergames345@gmail.com>
2022-07-06 10:35:07 +0200

    qwerty

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -11,19 +11,11 @@ const NavBar = () => {
         <label id="trigger" htmlFor="menu-toggle"></label>
         <label id="burger" htmlFor="menu-toggle"></label>
         <div className="links">
-<<<<<<< HEAD
-            <Link to='/'>Główna</Link>
-            <Link to='/order'>Zamów jedzenie</Link>
-            <Link to='/log-in'>Zaloguj się</Link>
-            <Link to='/admin-panel'>Panel administratora</Link>
-        </div> 
-=======
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/'>Główna</NavLink>
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/order'>Zamów jedzenie</NavLink>
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/log-in'>Zaloguj się</NavLink>
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/admin-panel'>Panel administratora</NavLink>
         </div>
->>>>>>> 4894627ec8ce3530a2e2664d2cf66495b56ee72f
     </div>
   )
 } 
