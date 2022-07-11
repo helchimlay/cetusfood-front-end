009ee542ff47cb369714cd7c5e9d154ea0c7aa58
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-11 13:39:51 +0200

    uwu

--- src/App.js
+++ src/App.js
@@ -1,44 +1,60 @@
-import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import OrderPanel from './components/OrderPanel/OrderPanel';
-import LogIn from './components/LogIn/LogIn';
-import Register from './components/Register/Register';
-import AdminPanel from './components/AdminPanel/AdminPanel';
-import HomePage from './components/HomePage/HomePage';
-import Footer from './components/Footer/Footer';
-import RestList from './components/AdminPanel/RestList/RestList';
-import NewRestForm from './components/AdminPanel/RestList/NewRestForm/NewRestForm';
-import AdminPanelStart from './components/AdminPanel/RestList/AdminPanelStart';
-import NotFound404 from './components/NotFound404/NotFound404';
-import EditRestForm from './components/AdminPanel/RestList/EditRestForm/EditRestForm';
-import {HelmetProvider } from "react-helmet-async";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import OrderPanel from "./components/OrderPanel/OrderPanel";
+import LogIn from "./components/LogIn/LogIn";
+import Register from "./components/Register/Register";
+import AdminPanel from "./components/AdminPanel/AdminPanel";
+import HomePage from "./components/HomePage/HomePage";
+import Footer from "./components/Footer/Footer";
+import RestList from "./components/AdminPanel/RestList/RestList";
+import NewRestForm from "./components/AdminPanel/RestList/NewRestForm/NewRestForm";
+import AdminPanelStart from "./components/AdminPanel/RestList/AdminPanelStart";
+import NotFound404 from "./components/NotFound404/NotFound404";
+import EditRestForm from "./components/AdminPanel/RestList/EditRestForm/EditRestForm";
+import { HelmetProvider } from "react-helmet-async";
+import React from "react";
+
+export const GlobalCtx = React.createContext();
+
 function App() {
+  const [user, setUser] = React.useState({ loggedIn: true, role: 'admin' });
+  const [accessToken, setAccessToken] = React.useState(null);
+
   return (
     <HelmetProvider>
       <Router>
-      <div className="App">
-        <NavBar />
-        <Routes>
-          <Route path='/' element={<HomePage />} exact />
-          <Route path='/order' element={<OrderPanel />} />
-          <Route path='/log-in' element={<LogIn />} />
-          <Route path='/register' element={<Register />} />
-          <Route path='/admin-panel' element={<AdminPanel />}>
-            <Route path='restaurants' element={<RestList />} />
-            <Route path='restaurants/add-new' element={<NewRestForm />} />
-            <Route path='restaurants/edit/:id' element={<EditRestForm />} />
-            <Route path='users' element={<h3 className="title">Lista użytkowników</h3>} />
-            <Route path='' element={<AdminPanelStart />} />
-          </Route>
-          <Route exact path='*' element={<NotFound404 />} />
-        </Routes>
-        <Footer />
-        {/* <Popup show={false}/> */}
-      </div>
-    </Router>
+        <div className="App">
+          <GlobalCtx.Provider value={{
+            user, setUser,
+            accessToken, setAccessToken
+          }}>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<HomePage />} exact />
+              {user.loggedIn && <Route path="/order" element={<OrderPanel />} />}
+              <Route path="/log-in" element={<LogIn />} />
+              <Route path="/register" element={<Register />} />
+              {user.role==='admin' && (
+                <Route path="/admin-panel" element={<AdminPanel />}>
+                <Route path="restaurants" element={<RestList />} />
+                <Route path="restaurants/add-new" element={<NewRestForm />} />
+                <Route path="restaurants/edit/:id" element={<EditRestForm />} />
+                <Route
+                  path="users"
+                  element={<h3 className="title">Lista użytkowników</h3>}
+                />
+                <Route path="" element={<AdminPanelStart />} />
+              </Route>
+              )}
+              <Route exact path="*" element={<NotFound404 />} />
+            </Routes>
+            <Footer />
+          </GlobalCtx.Provider>
+          {/* <Popup show={false}/> */}
+        </div>
+      </Router>
     </HelmetProvider>
-    
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,8 +2,11 @@ import React from 'react';
 import './NavBar.css';
 import { NavLink } from 'react-router-dom';
 import cetusik from '../../images/cetusFood2.png';
+import { GlobalCtx } from '../../App';
 
 const NavBar = () => {
+  const GCtx = React.useContext(GlobalCtx);
+
   return (
     <div className='NavBar'>
         <div className="logo"><img src={cetusik} alt="cetusik" /><h3>Cetus<span>Food</span></h3></div>
@@ -12,10 +15,10 @@ const NavBar = () => {
         <label id="burger" htmlFor="menu-toggle"></label>
         <div className="links">
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/'>Główna</NavLink>
-            <NavLink className={({isActive}) => isActive ? 'active' : null} to='/order'>Zamów jedzenie</NavLink>
+            {GCtx.user.loggedIn && <NavLink className={({isActive}) => isActive ? 'active' : null} to='/order'>Zamów jedzenie</NavLink>}
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/log-in'>Zaloguj się</NavLink>
             <NavLink className={({isActive}) => isActive ? 'active' : null} to='/register'>Załóż konto</NavLink>
-            <NavLink className={({isActive}) => isActive ? 'active' : null} to='/admin-panel'>Panel administratora</NavLink>
+            {GCtx.user.role==='admin' && <NavLink className={({isActive}) => isActive ? 'active' : null} to='/admin-panel'>Panel administratora</NavLink>}
         </div>
     </div>
   )
