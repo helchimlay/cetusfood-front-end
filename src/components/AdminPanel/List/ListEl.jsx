670dd44b7ed778efc853b94d1f7a2ec77f7fa44d
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-11 08:34:44 +0200

    a

--- src/components/AdminPanel/List/ListEl.jsx
+++ src/components/AdminPanel/List/ListEl.jsx
@@ -3,36 +3,36 @@ import { ListContext } from './List';
 import { useNavigate } from 'react-router-dom';
 
 const ListEl = ({ data, dontShow, i, setData }) => {
-    const Ctx = React.useContext(ListContext);
-    const inputRef = React.useRef();
-    const navigate = useNavigate();
+  const Ctx = React.useContext(ListContext);
+  const inputRef = React.useRef();
+  const navigate = useNavigate();
 
-    const handleCheckboxChange = () => {
-        if(inputRef.current.checked){
-            Ctx.setSelectedIds(prev => [...prev, data.id]);
-        }else{
-            Ctx.setSelectedIds(prev => prev.filter(val => val !== data.id));
-        }
+  const handleCheckboxChange = () => {
+    if (inputRef.current.checked) {
+      Ctx.setSelectedIds(prev => [...prev, data.id]);
+    } else {
+      Ctx.setSelectedIds(prev => prev.filter(val => val !== data.id));
     }
-    const handleDblClick = () => {
-        if(inputRef.current.checked){
-            Ctx.setSelectedIds(prev => [...prev, data.id]);
-            inputRef.current.click();
-        }else{
-            Ctx.setSelectedIds(prev => prev.filter(val => val !== data.id))
-            inputRef.current.click();
-        }
+  }
+  const handleDblClick = () => {
+    if (inputRef.current.checked) {
+      Ctx.setSelectedIds(prev => [...prev, data.id]);
+      inputRef.current.click();
+    } else {
+      Ctx.setSelectedIds(prev => prev.filter(val => val !== data.id))
+      inputRef.current.click();
     }
+  }
 
   return (
     <tr className={`ListEl ${Ctx.selectedIds.includes(data.id) ? 'checked' : null}`} onDoubleClick={handleDblClick}>
-        <td>{i+1}</td>
-        <td><input checked={Ctx.selectedIds.includes(data.id) ? true : false} ref={inputRef} onChange={handleCheckboxChange} type="checkbox" name="table-checkbox" /></td>
-        {Object.keys(data).map((key, i) => dontShow.includes(key) || <td key={i}>{data[key]}</td>)}
-        <td className='actions'>
-            <button className='edit-btn' onClick={() => navigate(`edit/${data.id}`)}><i className='bx bxs-edit'></i></button>
-            <button className='del-btn' onClick={() => {Ctx.deleteElFunc(data.id)}}><i className='bx bx-trash-alt' ></i></button>
-        </td>
+      <td>{i + 1}</td>
+      <td><input checked={Ctx.selectedIds.includes(data.id) ? true : false} ref={inputRef} onChange={handleCheckboxChange} type="checkbox" name="table-checkbox" /></td>
+      {Object.keys(data).map((key, i) => dontShow.includes(key) || <td key={i}>{data[key]}</td>)}
+      <td className='actions'>
+        <button className='edit-btn' onClick={() => navigate(`edit/${data.id}`)}><i className='bx bxs-edit'></i></button>
+        <button className='del-btn' onClick={() => { Ctx.deleteElFunc(data.id) }}><i className='bx bx-trash-alt' ></i></button>
+      </td>
     </tr>
   )
 }
--- src/components/OrderPanel/Stepper.jsx
+++ src/components/OrderPanel/Stepper.jsx
@@ -3,20 +3,26 @@ import './Stepper.css';
 import { OrderPnlCtx } from './OrderPanel';
 
 const Stepper = () => {
-    const Ctx = React.useContext(OrderPnlCtx);
+  const Ctx = React.useContext(OrderPnlCtx);
 
   return (
     <div className='Stepper'>
+      <div className="steps">
         <div className="line"></div>
         {Ctx?.steps.map((el, i) => (
-            <button key={i} onClick={() => Ctx.setStep(i)} className={Ctx?.step===i?'current':null}>
-              {i===0 && (Ctx.formData.restId ? <i className='bx bx-check'></i> : <>{i+1}</>)}
-              {i===1 && (Ctx.formData.products.length>0 ? <i className='bx bx-check'></i> : <>{i+1}</>)}
-              {i===2 && <>{i+1}</>}
-              <span>{el}</span>
-              
-            </button>
+          <button key={i} onClick={() => Ctx.setStep(i)} className={Ctx?.step === i ? 'current' : null}>
+            {i === 0 && (Ctx.formData.restId ? <i className='bx bx-check'></i> : <>{i + 1}</>)}
+            {i === 1 && (Ctx.formData.products.length > 0 ? <i className='bx bx-check'></i> : <>{i + 1}</>)}
+            {i === 2 && <>{i + 1}</>}
+            <span>{el}</span>
+          </button>
         ))}
+      </div>
+      <nav>
+        {Ctx.step!==0 && <button onClick={() => Ctx.setStep(prev => prev - 1)}><i class='bx bx-left-arrow-alt' ></i>Poprzedni krok</button>}
+        {Ctx.step!==2 && <div></div>}
+        {Ctx.step!==2 && <button onClick={() => Ctx.setStep(prev => prev + 1)}>Następny krok<i class='bx bx-right-arrow-alt' ></i></button>}
+      </nav>
     </div>
   )
 }
