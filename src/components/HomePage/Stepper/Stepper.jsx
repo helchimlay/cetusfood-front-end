396e52ce5b77fc08f9813f5de3436e53b4e7f802
Wojtek <weichergames345@gmail.com>
2022-07-11 10:53:56 +0200

    l

--- src/components/HomePage/Stepper/Stepper.jsx
+++ src/components/HomePage/Stepper/Stepper.jsx
@@ -1,38 +1,21 @@
-import React from 'react';
-import './Stepper.css';
+import React from "react";
+import "./Stepper.scss";
 
-const Stepper = () => {
-    return(
-        <div className="Stepper">
-            
-            <div className="line"></div>
-<div className='step'>
-    
-    <div className="one">
-    <p className="circle">1</p>
-    <p className="jeden">01.Krok pierwszy:<br/>
-    Otrzymujesz na  Mail wiadomość<br/>
-    z proźbą o złożenie zamówienia </p>
-</div>
-
-<div className="two">
-    <p className="circlee">2</p>
-        <p className="dwa">02.Krok drugi:<br/>
-            Wybierasz na aplikacji<br/>
-            co chcesz zamówić</p>
-</div>
-
-<div className="three">
-    <p className="circleee">3</p>
-        <p className="trzy">03.Krok trzeci:<br/>
-            Zamawiamy Ci co chcesz!</p>
-
-</div>
-</div>
-
-
-        </div>
-    )
-}
+const Stepper = ({ items }) => {
+  return (
+    <div className="Stepper">
+      {/* <div className="line" ></div> */}
+      <div className="step">
+        {items.map((el, i) => (
+          <div className={el.circContClass}>
+            <p className={el.circClass}>{el.num}</p>
+            <p className={el.circHeadingClass}>{el.heading}:</p>
+            <p className={el.circDescClass}>{el.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Stepper;
