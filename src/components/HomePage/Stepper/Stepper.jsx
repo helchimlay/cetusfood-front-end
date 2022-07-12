2bf1cac4e385d659c0d9a91355d4afb3a351e89f
Wojtek <weichergames345@gmail.com>
2022-07-12 08:47:30 +0200

    key={i}

--- src/components/HomePage/Stepper/Stepper.jsx
+++ src/components/HomePage/Stepper/Stepper.jsx
@@ -4,13 +4,13 @@ import "./Stepper.scss";
 const Stepper = ({ items }) => {
   return (
     <div className="Stepper">
-      {/* <div className="line" ></div> */}
+      <div className="line" ></div>
       <div className="step">
         {items.map((el, i) => (
-          <div className={el.circContClass}>
-            <p className={el.circClass}>{el.num}</p>
-            <p className={el.circHeadingClass}>{el.heading}:</p>
-            <p className={el.circDescClass}>{el.description}</p>
+          <div key={i} className='step-cont'>
+            <p className='circle'>{el.num}</p>
+            <p className='heading'>{el.heading}:</p>
+            <p className='desc'>{el.description}</p>
           </div>
         ))}
       </div>
