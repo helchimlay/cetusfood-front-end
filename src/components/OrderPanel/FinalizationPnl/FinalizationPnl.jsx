a62b63c97f272c16d35e2642215da262db18a859
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-13 09:16:26 +0200

    uwu

--- src/components/OrderPanel/FinalizationPnl/FinalizationPnl.jsx
+++ src/components/OrderPanel/FinalizationPnl/FinalizationPnl.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import './FinalizationPnl.css';
+import './FinalizationPnl.scss';
 import { OrderPnlCtx } from '../OrderPanel';
+import { addOrder } from '../../../services/RestaurationsList';
+import { GlobalCtx } from '../../../App';
 
 const FinalizationPnl = () => {
     const Ctx = React.useContext(OrderPnlCtx);
+    const { user } = React.useContext(GlobalCtx);
+
+    const order = () => {
+        let restId = Ctx.formData.restId;
+        let content = [];
+        Ctx.formData.products.forEach(el => content.push(el[0]));
+        content = content.join('|');
+        console.log({ restId, content })
+        addOrder(restId, content, user.accessToken);
+    }
 
   return (
     <div className='FinalizationPnl'>
-        www
+        <div>
+            <h4></h4>
+        </div>
+        <div>
+            <button type='button' onClick={order}>Potwierdź zamówienie</button>
+        </div>
     </div>
   )
 }
