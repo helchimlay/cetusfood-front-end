d39aeab02f828810454870bb7c8f7668a19395fb
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-13 09:45:29 +0200

    zamawianie chyba działą

--- src/components/OrderPanel/FinalizationPnl/FinalizationPnl.jsx
+++ src/components/OrderPanel/FinalizationPnl/FinalizationPnl.jsx
@@ -3,10 +3,12 @@ import './FinalizationPnl.scss';
 import { OrderPnlCtx } from '../OrderPanel';
 import { addOrder } from '../../../services/RestaurationsList';
 import { GlobalCtx } from '../../../App';
+import { useNavigate } from 'react-router-dom';
 
 const FinalizationPnl = () => {
     const Ctx = React.useContext(OrderPnlCtx);
     const { user } = React.useContext(GlobalCtx);
+    const navigate = useNavigate();
 
     const order = () => {
         let restId = Ctx.formData.restId;
@@ -15,15 +17,21 @@ const FinalizationPnl = () => {
         content = content.join('|');
         console.log({ restId, content })
         addOrder(restId, content, user.accessToken);
+        navigate('/');
     }
 
   return (
     <div className='FinalizationPnl'>
+        <img className='rest-logo' src={Ctx.restList.filter(v => v.id===Ctx.formData.restId)[0].logoUrl} />
         <div>
-            <h4></h4>
-        </div>
+            <h4>Restauracja: <span>{Ctx.restList.filter(v => v.id===Ctx.formData.restId)[0].name}</span></h4>
+            <div className='list'>
+                <h5>Lista produktów: </h5>
+                {Ctx.formData.products.map((el, i) => <p key={i}>{i+1}. {el[0]}</p>)}
+            </div>
         <div>
             <button type='button' onClick={order}>Potwierdź zamówienie</button>
+        </div>
         </div>
     </div>
   )
