b70477f3ed8f557065bf1e8819bf6223e2936f45
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-13 12:45:30 +0200

    zzzzzzzzz

--- src/components/AdminPanel/UsersList/UsersList.jsx
+++ src/components/AdminPanel/UsersList/UsersList.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import List from '../List/List';
+import { getAccounts, getAccountById, getAccountsByEmail, deleteAccount } from '../../../services/Users';
+import { useNavigate } from 'react-router-dom';
+import { GlobalCtx } from '../../../App';
 
 const UsersList = () => {
+    const navigate = useNavigate();
+    const { user } = React.useContext(GlobalCtx);
     return (<>
         <h3 className="title">Lista użytkowników</h3>
         
-        {/* <List
-            getDataFunc={getRestaurants}
-            getDataByParamFunc={getRestaurantsByName}
+        <List
+            getDataFunc={getAccounts}
+            getDataByParamFunc={getAccountsByEmail}
             dontShow={['id']}
-            deleteElFunc={deleteRestaurant}
+            deleteElFunc={deleteAccount}
             buttons={[
                 {text: 'Dodaj nowy element', onClick: () => navigate('add-new')}
             ]}
             accessToken={user.accessToken}
-        /> */}
+        />
     </>)
 }
 
--- src/services/Users.js
+++ src/services/Users.js
@@ -6,6 +6,16 @@ export const getAccounts = async (token) => {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
+export const getAccountById = async (id, token) => {
+  return await axios.get(`${variables.proxy}/admin/account/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+export const getAccountsByEmail = async (email, token) => {
+  return await axios.get(`${variables.proxy}/admin/account/email/${email}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
 export const deleteAccount = async (id, token) => {
   return await axios.delete(`${variables.proxy}/admin/account/${id}`, {
     headers: { Authorization: `Bearer ${token}` },
