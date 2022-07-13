c2bce686c778907e9a8682fc5b7b89e5362ef5e8
Rafal Krupa <rafal.gabriel.kr@gmail.com>
2022-07-13 12:11:06 +0200

    a

---
+++ src/components/AdminPanel/UsersList/UsersList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import List from '../List/List';
+
+const UsersList = () => {
+    return (<>
+        <h3 className="title">Lista użytkowników</h3>
+        
+        {/* <List
+            getDataFunc={getRestaurants}
+            getDataByParamFunc={getRestaurantsByName}
+            dontShow={['id']}
+            deleteElFunc={deleteRestaurant}
+            buttons={[
+                {text: 'Dodaj nowy element', onClick: () => navigate('add-new')}
+            ]}
+            accessToken={user.accessToken}
+        /> */}
+    </>)
+}
+
+export default UsersList;
---
+++ src/services/Users.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+import variables from "../assets/variables.json";
+
+export const getAccounts = async (token) => {
+  return await axios.get(`${variables.proxy}/admin/account`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
+export const deleteAccount = async (id, token) => {
+  return await axios.delete(`${variables.proxy}/admin/account/${id}`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+};
